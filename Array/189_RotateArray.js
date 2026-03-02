b73bd146d84dbbfb5b5e7e324ef7b5722be3668f
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-02 19:01:08 +0530

    7th leetcode solution

--- Array/189_RotateArray.js
+++ Array/189_RotateArray.js
@@ -11,3 +11,15 @@ var rotate = function (nums, k) {
     nums[0] = value;
   }
 };
+
+
+// 2nd approach with extra space BigO(1)extra space
+var rotate = function (nums, k) {
+  let temp = new Array(nums.length);
+  for (let i = 0; i < nums.length; i++) {
+    temp[(i + k) % nums.length] = nums[i];
+  }
+  for (let i = 0; i < temp.length; i++) {
+    nums[i] = temp[i];
+  }
+};
