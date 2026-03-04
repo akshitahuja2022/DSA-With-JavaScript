7901d919d244c0db4674228301259c283929e4cd
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-04 17:21:35 +0530

    8th leetcode question

--- Array/283_MovesZeroes.js
+++ Array/283_MovesZeroes.js
@@ -1,6 +1,6 @@
 // 283. Move Zeroes
 
-// Brute Force - O(n) + O(x) + O(n-x) = O(2n) -> O(n) with extra space
+// Brute Force - O(n) + O(x) + O(n-x) = O(2n) -> O(n) with extra space O(n) worst case
 var moveZeroes = function(nums) {
   let k = 0;
   let temp = [];
@@ -19,3 +19,40 @@ var moveZeroes = function(nums) {
   }
 
 };
+
+// better version - O(x) + O(n-x) = O(n) no extra space = O(1)
+var moveZeroes = function(nums) {
+let j = -1;
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === 0) {
+    j = i;
+    break;
+  }
+}
+
+if(j ===-1) return;
+
+for (let i = j + 1; i < nums.length; i++) {
+  if (nums[i] !== 0) {
+    let temp = nums[j];
+    nums[j] = nums[i];
+    nums[i] = temp;
+    j++;
+  }
+}
+};
+
+// optimal - O(n)
+var moveZeroes = function(nums) {
+let j = 0;
+
+for (let i=0; i < nums.length; i++) {
+  if (nums[i] !== 0) {
+    let temp = nums[j];
+    nums[j] = nums[i];
+    nums[i] = temp;
+    j++;
+  }
+}
+}
