2970b7bad8956550f476270d28f69e9a2e346686
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-04 16:56:40 +0530

    8th leetcode Solution

--- Array/283_MovesZeroes.js
+++ Array/283_MovesZeroes.js
@@ -1,18 +1,21 @@
 // 283. Move Zeroes
 
-var moveZeroes = function (nums) {
-  let i = 0;
-  let j = 0;
-
-  while (i < nums.length) {
-    if (nums[i] === 0) {
-      i++;
-    } else {
-      let temp = nums[i];
-      nums[i] = nums[j];
-      nums[j] = temp;
-      i++;
-      j++;
+// Brute Force - O(n) + O(x) + O(n-x) = O(2n) -> O(n) with extra space
+var moveZeroes = function(nums) {
+  let k = 0;
+  let temp = [];
+  for(let i=0; i<nums.length; i++){
+    if(nums[i]!==0){
+        temp[k] = nums[i]
+        k++
     }
   }
+  for(let i=0; i<temp.length; i++){
+    nums[i] = temp[i]
+  }
+
+  for(let i = temp.length; i<nums.length; i++){
+    nums[i] = 0
+  }
+
 };
