c7f85ebd7425e0651107942c58edf71e7271366c
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-08 17:11:15 +0530

    5th leetcode question

--- Array/53_MaxSubArray.js
+++ Array/53_MaxSubArray.js
@@ -1,5 +1,22 @@
 // 53 Maximum SubArray
 
+//  Brute force method - O(n^2) and it not works for [-1]
+var maxSubArray = (nums, k) => {
+  let maxSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+
+      if (sum > maxSum) {
+        maxSum = sum;
+      }
+    }
+  }
+  return maxSum;
+};
+
+// optimal solution - O(n)
 var maxSubArray = function (nums) {
   let maxSum = -Infinity;
   let sum = 0;
