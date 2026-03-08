1325cfabb8454d812984f8df9353867fecb744a6
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-08 16:20:45 +0530

    6th leetcode question

--- Array/169_MajorityElement.js
+++ Array/169_MajorityElement.js
@@ -1,5 +1,28 @@
 // 169 Majority Element
 
+/* The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the 
+majority element always exists in the array. 
+
+if n = 7
+(n/2) = 7/2 = 3 
+ [2,2,1,1,1,2,2] so in case -- 2 is majority because it apperar 4 time and in question 4>3
+*/
+
+// Brute Force method - O(n^2)
+var majorityElement = function (nums) {
+  let n = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    var count = 0;
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] === nums[j]) count++;
+    }
+    if (count > n / 2) return nums[i];
+  }
+};
+
+// Better Solution - hashing O(n logn)
+
+// Optimal Solution O(n)
 var majorityElement = function (nums) {
   let ans = nums[0];
   let i = 0;
