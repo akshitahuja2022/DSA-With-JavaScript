09924cf74d77d9734cff1cc78727661c6d19ba90
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-08 15:51:26 +0530

    4th leetcode question

--- Array/75_SortColors.js
+++ Array/75_SortColors.js
@@ -1,5 +1,34 @@
 // 75 Sort Colors
 
+// Better Solution - O(n) + O(n) - O(n) and O(1) space complexity
+var sortColors = function (nums) {
+  let count0 = 0;
+  let count1 = 0;
+  let count2 = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      count0++;
+    } else if (nums[i] === 1) {
+      count1++;
+    } else {
+      count2++;
+    }
+  }
+  for (let i = 0; i < count0; i++) {
+    nums[i] = 0;
+  }
+
+  for (let j = count0; j < count0 + count1; j++) {
+    nums[j] = 1;
+  }
+
+  for (let k = count0 + count1; k < nums.length; k++) {
+    nums[k] = 2;
+  }
+};
+
+// Optimal Solution O(n) and O(1) spcar complexity
 var sortColors = function (nums) {
   let i = 0;
   let j = 0;
