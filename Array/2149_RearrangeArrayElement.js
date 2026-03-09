62199e91e36fd770645796c445908584b918a284
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-09 16:57:10 +0530

    16th leetcode question

--- Array/2149_RearrangeArrayElement.js
+++ Array/2149_RearrangeArrayElement.js
@@ -21,4 +21,22 @@ var rearrangeArray = function (nums) {
   return nums;
 };
 
+// Optimal Solution - O(n) and space O(n)
+var rearrangeArray = function (nums) {
+let ans = new Array(nums.length).fill(0);
+  let k = 0;
+  let j = 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      ans[k] = nums[i];
+      k += 2;
+    } else {
+      ans[j] = nums[i];
+      j += 2;
+    }
+  }
+
+  return ans;
+};
+
 console.log(rearrangeArray(nums));
