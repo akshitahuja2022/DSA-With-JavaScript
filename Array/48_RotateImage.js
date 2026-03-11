c8555ae1e1e9b6974d53579c9fbda70a40bc0406
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-11 18:43:02 +0530

    22th leetcode question

--- Array/48_RotateImage.js
+++ Array/48_RotateImage.js
@@ -9,19 +9,38 @@ let matrix = [
 ];
 
 // Brute force solution - O(n²) + O(n²) = O(n²) and O(n^2) space complexity
+// var rotate = function (matrix) {
+//   let n = matrix.length;
+//   let ans = Array.from({ length: n }, () => new Array(n));
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n; j++) {
+//       ans[j][n - 1 - i] = matrix[i][j];
+//     }
+//   }
+
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n; j++) {
+//       matrix[i][j] = ans[i][j];
+//     }
+//   }
+// };
+
+// Optimal Solution - O(n^2)+ O(n) and space O(1)
 var rotate = function (matrix) {
   let n = matrix.length;
-  let ans = Array.from({ length: n }, () => new Array(n));
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      ans[j][n - 1 - i] = matrix[i][j];
+
+  // Transpose the matrix
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = i + 1; j < n; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
     }
   }
 
+  // reverse the row
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      matrix[i][j] = ans[i][j];
-    }
+    matrix[i].reverse();
   }
 };
 
