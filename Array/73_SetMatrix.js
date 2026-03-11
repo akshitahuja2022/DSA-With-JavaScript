7a3f7dad110cdb4872d891886bd259dddc20c227
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-11 17:02:53 +0530

    21th leetcode question

--- Array/73_SetMatrix.js
+++ Array/73_SetMatrix.js
@@ -2,16 +2,16 @@
 
 // Brute force solution - O(n*m*(n+m))
 var setZeroes = function (matrix) {
-    let m = matrix.length;
-    let n = matrix[0].length
+  let m = matrix.length;
+  let n = matrix[0].length;
 
-    const MARK = Number.MIN_SAFE_INTEGER
+  const MARK = Number.MIN_SAFE_INTEGER;
 
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (matrix[i][j] === 0) {
-        markRow(matrix,i,n, MARK);
-        markColumn(matrix,j,m, MARK);
+        markRow(matrix, i, n, MARK);
+        markColumn(matrix, j, m, MARK);
       }
     }
   }
@@ -25,7 +25,7 @@ var setZeroes = function (matrix) {
   }
 };
 
-function markRow(matrix,i,n, MARK) {
+function markRow(matrix, i, n, MARK) {
   for (let j = 0; j < n; j++) {
     if (matrix[i][j] !== 0) {
       matrix[i][j] = MARK;
@@ -39,3 +39,29 @@ function markColumn(matrix, j, m, MARK) {
     }
   }
 }
+
+// Better Solution Solution- O(2*n*m) and space O(n) + O(m)
+var setZeroes = function (matrix) {
+  let m = matrix.length; // row
+  let n = matrix[0].length; // col
+
+  let row = new Array(m.length).fill(0);
+  let col = new Array(n.length).fill(0);
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === 0) {
+        row[i] = 1;
+        col[j] = 1;
+      }
+    }
+  }
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (row[i] || col[j]) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+};
