080862a13cd161bc39d1a2c67db9b9c0971b9486
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-12 18:01:37 +0530

    24th leetcode question

--- Array/560_SubArraySumK.js
+++ Array/560_SubArraySumK.js
@@ -15,5 +15,22 @@ var subarraySum = function (nums, k) {
   return count;
 };
 
+// Optimal Solution - O(n) and space O(n)
+var subarraySum = function (nums, k) {
+  let map = new Map();
+  map.set(0, 1);
+  let prefixSum = 0;
+  let count = 0;
+  for (let num of nums) {
+    prefixSum += num;
+
+    if (map.has(prefixSum, k)) {
+      count += map.get(prefixSum - k);
+    }
+    map.set(prefixSum, (map.get(prefixSum) || 0) + 1);
+  }
+  return count;
+};
+
 nums = [1, -1, 0];
 console.log(subarraySum(nums, 0));
