3a38a1cf92bd1efe2a5fa8ec84fcb28d7dd94c89
Akshit Ahuja <akshitahuja1322@gmail.com>
2026-03-13 17:27:19 +0530

    25th leetcode question

--- Array/167_TwoSum!!.js
+++ Array/167_TwoSum!!.js
@@ -10,3 +10,30 @@ var twoSum = function (numbers, target) {
     }
   }
 };
+
+// Brute Force Solution - O(n^2)
+// var twoSum = function(numbers, target) {
+    
+//     for(let i=0; i<numbers.length; i++){
+//         for(let j=i+1; j<numbers.length; j++){
+//             if(numbers[i]+numbers[j] === target){
+//                 return [i+1,j+1]
+//             }
+//         }
+//     }
+// };
+
+// Better Solution - O(n) and O(n)
+var twoSum = function(numbers, target) {
+    let map = new Map();
+    for(let i=0; i<numbers.length; i++){
+       let compliment = target - numbers[i];
+
+       if(map.has(compliment)){
+        return [map.get(compliment), i+1]
+       }
+
+       map.set(numbers[i], i+1)
+        
+    }
+};
